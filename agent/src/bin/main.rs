e5a70379fbc1102b46ac491a812ee5ac6e8c9c72
mdacosta <dacostab@insa-toulouse.fr>
2025-07-24 11:07:44 +0200

    feat(plugin-quarch): adding a plugin to measure consumption of a disk

--- agent/src/bin/main.rs
+++ agent/src/bin/main.rs
@@ -51,6 +51,7 @@ fn load_plugins_metadata() -> Vec<PluginMetadata> {
             plugin_procfs::ProcfsPlugin,
             plugin_nvidia_nvml::NvmlPlugin,
             plugin_nvidia_jetson::JetsonPlugin,
+	    plugin_quarch::QuarchPlugin,
         ]);
     }
 
